76b16b11b5eff7a812d7ed10f04c0b2a39022149
migueliiin <91721552+MiguelGG03@users.noreply.github.com>
2023-11-06 23:12:11 +0100

    Refactor code for improved readability and performance. Copilot

---
+++ CreacionYGestionDePizzas/pizzaBuilder.py
@@ -0,0 +1,15 @@
+class PizzaBuilder:
+    def __init__(self):
+        self.pizza = Pizza()
+
+    def setDough(self, dough):
+        self.pizza.dough = dough
+
+    def setSauce(self, sauce):
+        self.pizza.sauce = sauce
+
+    def setTopping(self, topping):
+        self.pizza.topping = topping
+
+    def getPizza(self):
+        return self.pizza
