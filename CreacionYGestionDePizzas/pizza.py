c71620b755b535a0e1b94f5f8da7b65c7b445fee
migueliiin <91721552+MiguelGG03@users.noreply.github.com>
2023-11-07 15:56:37 +0100

    Falta un database de cosas para poder verificar

--- CreacionYGestionDePizzas/pizza.py
+++ CreacionYGestionDePizzas/pizza.py
@@ -1,8 +1,33 @@
 class Pizza:
     def __init__(self):
-        self.dough = ""
-        self.sauce = ""
-        self.topping = ""
+        self._dough = ""
+        self._sauce = ""
+        self._topping = ""
+        self._pairing = ""
 
+    @property
+    def dough(self):
+        return self._dough
+    
+    @dough.setter
+    def dough(self, dough):
+        self._dough = dough
+
+    @property
+    def sauce(self):
+        return self._sauce
+    
+    @sauce.setter
+    def sauce(self, sauce):
+        self._sauce = sauce
+
+    @property
+    def topping(self):
+        return self._topping
+    
+    @topping.setter
+    def topping(self, topping):
+        self._topping = topping
+        
     def __str__(self):
         return f"dough: {self.dough}\nsauce: {self.sauce}\ntopping: {self.topping}"
--- CreacionYGestionDePizzas/pizzaBuilder.py
+++ CreacionYGestionDePizzas/pizzaBuilder.py
@@ -1,3 +1,7 @@
+import sys
+sys.path.append('../')
+from pizza import Pizza
+
 class PizzaBuilder:
     def __init__(self):
         self.pizza = Pizza()
