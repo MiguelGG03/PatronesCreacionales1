9d57d589f536f3b0732f80e85425f2a1df5f269f
migueliiin <91721552+MiguelGG03@users.noreply.github.com>
2023-11-07 21:08:39 +0100

    Cambios importantes en el funcionamiento de la p

--- CreacionYGestionDePizzas/validator.py
+++ CreacionYGestionDePizzas/validator.py
@@ -1,7 +1,39 @@
 from abc import ABC, abstractmethod
+import csv
+from config import CARTA_PATH, INGREDIENTES , SALSAS
 
-class Validator(ABC):
+class Validador(ABC):
 
     @abstractmethod
-    def validate(self,*args,**kwargs):
-        pass    
+    def validar(self,*args,**kwargs):
+        pass
+
+class ValidarPizza(Validador):
+
+    def validar(self,pizza):
+        with open(CARTA_PATH, newline='') as File:  
+            reader = csv.reader(File,delimiter=';')
+            for row in reader:
+                if row[0] == pizza:
+                    return True
+            return False
+
+class ValidarIngredientes(Validador):
+    """
+    Valida que los ingredientes sean validos
+    """
+    def validar(self,ingrediente):
+        for _ingrediente in INGREDIENTES:
+            if _ingrediente == ingrediente:
+                return True
+        return False
+
+class ValidarSalsa(Validador):
+    """
+    Valida que la salsa sea valida
+    """
+    def validar(self,salsa):
+        for _salsa in SALSAS:
+            if _salsa == salsa:
+                return True
+        return False
