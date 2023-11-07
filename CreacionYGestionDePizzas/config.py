426262704a6aa6c3ade2baeb61d8ba4601767e63
migueliiin <91721552+MiguelGG03@users.noreply.github.com>
2023-11-07 16:05:21 +0100

    esto me viene de locos ajajajajajaj

---
+++ CreacionYGestionDePizzas/config.py
@@ -0,0 +1,8 @@
+CARTA_PATH = "data/carta.csv"
+
+INGREDIENTES = ["tomate","queso","queso azul","champiñones",
+                "bacon","pollo","pimiento",
+                "cebolla","piña","jamón",
+                "aceitunas","atún"]
+
+SALSAS = ["tomate","barbacoa","carbonara","pesto","pomodoro"]
