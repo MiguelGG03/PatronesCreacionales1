03bc2552d0f434484e36be33f0698438daf83905
migueliiin <91721552+MiguelGG03@users.noreply.github.com>
2023-11-07 16:06:00 +0100

    Voy a volar por el hiper espacio

--- CreacionYGestionDePizzas/config.py
+++ CreacionYGestionDePizzas/config.py
@@ -6,3 +6,7 @@ INGREDIENTES = ["tomate","queso","queso azul","champiñones",
                 "aceitunas","atún"]
 
 SALSAS = ["tomate","barbacoa","carbonara","pesto","pomodoro"]
+
+MASAS = ["fina","normal","gorda"]
+
+TAMAÑOS = ["pequeña","mediana","familiar"]
