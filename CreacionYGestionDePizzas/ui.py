fde53fda58fc00825667921fb7b707d6753b0d38
migueliiin <91721552+MiguelGG03@users.noreply.github.com>
2023-11-08 19:03:13 +0100

    Ya está, si soy la leche se dice y .

--- CreacionYGestionDePizzas/ui.py
+++ CreacionYGestionDePizzas/ui.py
@@ -1,106 +1,111 @@
 import sys
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget,
-                             QLabel, QComboBox, QLineEdit, QMessageBox, QFormLayout, QSpinBox)
+                             QLabel, QComboBox, QLineEdit, QMessageBox, QFormLayout, QCheckBox)
 sys.path.append("../")
-from CSVstorage import CSVstorage  # Asegúrate de que esta clase tenga métodos load() y save()
+from CSVstorage import CSVstorage
 from waiter import Waiter
 from pizzaBuilder import *
-from config import PEDIDOS_PATH, TAMANOS, MASAS, INGREDIENTES, SALSAS  # Constantes definidas en config.py
+from config import PEDIDOS_PATH, TAMANOS, MASAS, INGREDIENTES, SALSAS
 
 class PizzaOrderApp(QMainWindow):
     def __init__(self):
         super().__init__()
         self.storage = CSVstorage(PEDIDOS_PATH)
         self.waiter = Waiter()
+        self.selected_ingredients = []
         self.initUI()
 
     def initUI(self):
         self.setWindowTitle('Pizza Builder App')
         self.setGeometry(100, 100, 800, 600)
-
         layout = QVBoxLayout()
 
-        # Selector de pizza
         self.pizza_combo = QComboBox()
         self.pizza_combo.addItems(["Cuatro Quesos", "Barbacoa", "Margarita", "Personalizada"])
         self.pizza_combo.currentIndexChanged.connect(self.toggle_personalized_options)
         layout.addWidget(self.pizza_combo)
 
-        # Campos para la pizza personalizada
-        self.personalized_layout = QFormLayout()
+        self.size_combo = QComboBox()
+        self.size_combo.addItems(TAMANOS)
+        layout.addWidget(self.size_combo)
 
-        # Campo para el nombre de la pizza
+        self.dough_combo = QComboBox()
+        self.dough_combo.addItems(MASAS)
+        layout.addWidget(self.dough_combo)
+
+        self.create_personalized_options(layout)
+
+        self.order_button = QPushButton('Hacer pedido')
+        self.order_button.clicked.connect(self.place_order)
+        layout.addWidget(self.order_button)
+
+        central_widget = QWidget()
+        central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
+
+    def create_personalized_options(self, layout):
+        self.personalized_layout = QFormLayout()
         self.pizza_name_input = QLineEdit()
         self.pizza_name_input.setPlaceholderText("Nombre de la pizza (4-16 caracteres)")
         self.personalized_layout.addRow("Nombre:", self.pizza_name_input)
 
-        # Selector de ingredientes
-        self.ingredients_combo = QComboBox()
-        self.ingredients_combo.addItems(INGREDIENTES)
-        self.selected_ingredients = []
-        self.ingredients_combo.currentIndexChanged.connect(self.add_ingredient)
-        self.personalized_layout.addRow("Ingredientes:", self.ingredients_combo)
+        for ingrediente in INGREDIENTES:
+            cb = QCheckBox(ingrediente)
+            cb.stateChanged.connect(self.update_ingredient_selection)
+            self.personalized_layout.addRow(cb)
 
-        # Contador de ingredientes seleccionados
-        self.ingredients_count = QSpinBox()
-        self.ingredients_count.setRange(0, 3)
-        self.ingredients_count.setReadOnly(True)
-        self.personalized_layout.addRow("Seleccionados:", self.ingredients_count)
-
-        # Selector de salsas
         self.sauce_combo = QComboBox()
         self.sauce_combo.addItems(SALSAS)
         self.personalized_layout.addRow("Salsas:", self.sauce_combo)
 
-        # Inicialmente ocultar opciones personalizadas
         self.personalized_options = QWidget()
         self.personalized_options.setLayout(self.personalized_layout)
         self.personalized_options.setVisible(False)
         layout.addWidget(self.personalized_options)
 
-        # Continuación de la UI...
-
     def toggle_personalized_options(self, index):
-        # Mostrar u ocultar las opciones personalizadas según la selección
         is_personalized = self.pizza_combo.currentText() == "Personalizada"
         self.personalized_options.setVisible(is_personalized)
 
-    def add_ingredient(self, index):
-        ingredient = self.ingredients_combo.currentText()
-        if ingredient not in self.selected_ingredients:
-            if self.ingredients_count.value() < 3:
-                self.selected_ingredients.append(ingredient)
-                self.ingredients_count.setValue(self.ingredients_count.value() + 1)
-            else:
-                QMessageBox.warning(self, 'Advertencia', 'Solo puedes seleccionar hasta 3 ingredientes.')
-
-    # Continuación de los métodos...
-
-# Continuación de la aplicación...
+    def update_ingredient_selection(self, state):
+        source = self.sender()
+        if state == Qt.Checked and len(self.selected_ingredients) < 3:
+            self.selected_ingredients.append(source.text())
+        elif state == Qt.Unchecked and source.text() in self.selected_ingredients:
+            self.selected_ingredients.remove(source.text())
+        else:
+            source.setChecked(False)
+            QMessageBox.warning(self, 'Advertencia', 'Solo puedes seleccionar hasta 3 ingredientes.')
 
     def place_order(self):
-        # Obtener las selecciones del usuario
         pizza_type = self.pizza_combo.currentText()
         pizza_size = self.size_combo.currentText()
         pizza_dough = self.dough_combo.currentText()
-
-        # Lógica para construir la pizza
         if pizza_type == "Cuatro Quesos":
             self.waiter.pizzaBuilder = CuatroQuesosPizzaBuilder()
         elif pizza_type == "Barbacoa":
             self.waiter.pizzaBuilder = BarbacoaPizzaBuilder()
         # ... etc ...
 
-        # Aquí asumimos que la lógica de buildPizza ha sido adaptada para trabajar con PyQt
-        self.waiter.buildPizza(pizza_size, pizza_dough)
-        pizza = self.waiter.getPizza()
+        if pizza_type == "Personalizada":
+            pizza_name = self.pizza_name_input.text()
+            if not (4 <= len(pizza_name) <= 16):
+                QMessageBox.warning(self, 'Error', 'El nombre de la pizza debe tener entre 4 y 16 caracteres.')
+                return
+            if len(self.selected_ingredients) == 0:
+                QMessageBox.warning(self, 'Error', 'Debes seleccionar al menos un ingrediente.')
+                return
+            sauce = self.sauce_combo.currentText()
+            self.waiter.pizzaBuilder = PersonalizadaPizzaBuilder()
+            self.waiter.buildPizza(pizza_name, pizza_size, pizza_dough, sauce, self.selected_ingredients)
+        else:
+            self.waiter.buildPizza(pizza_size, pizza_dough)
 
-        # Guardar la pizza en la base de datos
+        pizza = self.waiter.getPizza()
         data = self.storage.load()
         data.append(pizza.to_csv())
         self.storage.save(data)
-
-        # Confirmar el pedido al usuario
         QMessageBox.information(self, 'Pedido', 'Tu pizza ha sido pedida con éxito!')
 
 def ui():
